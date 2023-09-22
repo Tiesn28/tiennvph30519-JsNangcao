e7e4a5e7d75945e1bd5e67473c69ed92627106ab
tiesn28 <tiennvph30519@fpt.edu.vn>
2023-09-22 14:03:29 +0700

    test commit

---
+++ section9/udemy/103.DestructuringArrays/script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+
+// const restaurant = {
+//     name: 'Classico Italiano',
+//     location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//     categories: ['Italian', 'Fizzeria', 'Vegetarian', 'Organic'],
+//     starterMenu: ['Focaccian', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+// }
